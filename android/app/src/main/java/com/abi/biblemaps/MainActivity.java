c693c23e93bf5c739674a96405f58112b75d9a19
abi-mam <abdachisa.dugasa@gmail.com>
2025-09-23 00:02:11 +0100

    fix: general update

--- android/app/src/main/java/com/abi/biblemaps/MainActivity.java
+++ android/app/src/main/java/com/abi/biblemaps/MainActivity.java
@@ -1,14 +1,33 @@
 package com.abi.biblemaps;
 
 import android.os.Bundle;
+import android.os.Build;
+import android.view.Window;
+import android.view.WindowInsetsController;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Manually register the Immersive plugin
+        Window window = getWindow();
+        int statusColor = getResources().getColor(R.color.colorBibleMapsDark);
+        window.setStatusBarColor(statusColor);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            WindowInsetsController controller = window.getInsetsController();
+            if (controller != null) {
+                controller.setSystemBarsAppearance(
+                    0, // clear APPEARANCE_LIGHT_STATUS_BARS
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                );
+            }
+        }
+
+        // 👇 Manually register Immersive plugin here
         registerPlugin(ImmersivePlugin.class);
     }
 }
